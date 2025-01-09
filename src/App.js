4b678d0bd0f4b6999f5b90487d0182a650d04c7e
Darshan <darshann679596@gmail.com>
2025-01-09 16:49:57 +0530

    origin main

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-import XTable from "./Components/xtable"
+import XTable from "./Components/xtable/XTable"
 const App = () =>{
     return(
         <>
-        
+
             <XTable/>
         </>
     )
